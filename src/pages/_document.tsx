784db581749424e8da870d2122d95c59bf5cfdd0
Marcos Kenji Kuribayashi <marcosken13@gmail.com>
2022-12-21 21:51:14 -0300

    chore: add initial settings

---
+++ src/pages/_document.tsx
@@ -0,0 +1,21 @@
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html lang="pt-BR">
+      <Head>
+        <link rel="shortcut icon" href="/favicon.png" type="image/png" />
+
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Cairo:wght@400;700&family=Inter:wght@400;700;900&family=Poppins:wght@400;500;700;900&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
